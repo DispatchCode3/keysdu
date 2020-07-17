d09133cb203cec00b7df2462217f43adc1a8ee9c
Rob Beuse <rbeuse911@gmail.com>
2020-07-17 02:18:04 +0000

    updates

--- keysdu/settings.py
+++ keysdu/settings.py
@@ -43,6 +43,17 @@ DATABASES = {
         'PORT':''
     }
 }
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'KeysDu <noreply@keysdu.com>'
+EMAIL_SUBJECT_PREFIX = '[KeysDu] '
+
 # Application definition
 
 INSTALLED_APPS = [
